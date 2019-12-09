6cb6a3541d778b7c3ced6281c185b355757132b6
aniuul <anna.golabek04@gmail.com>
2019-12-09 22:00:32 +0100

    1 file modified

--- src/app/hello.component.ts
+++ src/app/hello.component.ts
@@ -2,7 +2,7 @@ import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'hello',
-  template: "./hello.component.html,
+  template: "./hello.component.html",
   styles: [`./hello.component.css`]
 })
 export class HelloComponent  {
